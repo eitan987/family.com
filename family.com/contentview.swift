d75ad16e146345d4456c92d6f4ed64da78c68abb
eitan shlomo hi yifrach <eitanshlomohiyifrach@Mac.lan>
2025-04-01 16:56:21 +0300

    גרסה ראשונה של האתר

---
+++ family.com/contentview.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://google.com")!)
+            .ignoresSafeArea()
+    }
+}
---
+++ family.com/family.comTests/family_comTests.swift
@@ -0,0 +1,17 @@
+//
+//  family_comTests.swift
+//  family.comTests
+//
+//  Created by eitan shlomo hi yifrach on 31/03/2025.
+//
+
+import Testing
+@testable import family_com
+
+struct family_comTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
