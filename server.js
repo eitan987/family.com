52f4768ac30604ddcb1d102d076fb5ac3fd26533
eitan shlomo hi yifrach <eitanshlomohiyifrach@Mac.lan>
2025-04-04 16:11:58 +0300

    עדכון עיצוב והוספת קוד ל־login

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ import { getDatabase, ref, set, get } from 'firebase/database';
 // Your Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCzSULY0lOBQbKKvYq9vQeuELNeIQVjaao",
-  authDomain: "family-f4d22.firebaseapp.com",
-  projectId: "family-f4d22",
-  storageBucket: "family-f4d22.firebasestorage.app",
-  messagingSenderId: "676033750020",
-  appId: "1:676033750020:web:1546494d9f159d951c8151",
-  measurementId: "G-740KXJPEJ1"
+    authDomain: "family-f4d22.firebaseapp.com",
+    databaseURL: "https://family-f4d22-default-rtdb.firebaseio.com",
+    projectId: "family-f4d22",
+    storageBucket: "family-f4d22.firebasestorage.app",
+    messagingSenderId: "676033750020",
+    appId: "1:676033750020:web:1546494d9f159d951c8151",
+    measurementId: "G-740KXJPEJ1"
 };
 
 // Initialize Firebase
@@ -18,6 +19,9 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 let familyId = localStorage.getItem("familyId") || "demo";
+let tasks = [];
+let events = [];
+let messages = [];
 
 async function loadData() {
     try {
